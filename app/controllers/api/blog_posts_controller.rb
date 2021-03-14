4dd9252cc824546ac909b8450ce9fba0a5f4b09f
austin shields <ajshields196@gmail.com>
2021-03-14 10:42:33 -0400

    add blog_posts create route

--- app/controllers/api/blog_posts_controller.rb
+++ app/controllers/api/blog_posts_controller.rb
@@ -8,4 +8,24 @@ class Api::BlogPostsController < ApplicationController
     @blog_posts = BlogPost.all
     render "index.json.jb"
   end
+
+  def create
+    @blog_post = BlogPost.new(
+      title: params[:title],
+      text: params[:text]
+    )
+    @blog_post.save
+    if params[:tags]
+      tags = params[:tags].split(", ")
+      tags.each do |tag|
+        bpt = BlogPostTag.new(
+          blog_post_id: @blog_post.id,
+          tag_id: Tag.find_by(name: tag).id
+        )
+        bpt.save
+      end
+    end
+
+    render "show.json.jb"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   namespace :api do
     get "/blog_posts" => "blog_posts#index"
     get "/blog_posts/:id" => "blog_posts#show"
+    post "blog_posts" => "blog_posts#create"
   end
 end
